db45949a7cde1f4f9b6659d6dec6fb7c5e123961
Kristen San Martin <ksanmartin@filamentgames.com>
2023-11-06 13:13:50 -0800

    11-06 - addToSprint - format bundled tickets

--- addToSprint.js
+++ addToSprint.js
@@ -8,7 +8,7 @@ const BASE_RESOURCES_FOLDER = `D:\\Users\\Filament_Games\\Documents\\ProjectReso
 // const PROJECT_FOLDER= `${BASE_RESOURCES_FOLDER}\\CASA\\Tickets`
 const PROJECT_FOLDER = `${BASE_RESOURCES_FOLDER}\\FGW\\Tickets`;
 
-const newTickets = ["Defect/CSAR-952-CSAR-954/WSOD-defensive-approach"];
+const newTickets = ["Defect/CSAR-952/WSOD-defensive-approach"];
 
 function categorize(string, folders) {
   let entry = string;
@@ -17,16 +17,25 @@ function categorize(string, folders) {
     `::${ticket.toLowerCase().split("-").join("")}${type}::`;
 
   if (entry.includes("/")) {
+    // Entry is a git style string
     entry = entry.split("/");
     let [type, ticket, description] = entry;
     const file = `${ticket} - ${description.split("-").join(" ")}`;
     folders.push(`${PROJECT_FOLDER}\\"${file}"`);
     console.log(`# ${date.toISOString().split("T")[0]}`);
-    console.log(`${hotstring(ticket, "file")}${file}`);
-    console.log(`${hotstring(ticket, "git")}${string}`);
-    console.log(`${hotstring(ticket)}${ticket}`);
+
+    // Determine if ticket is bundled based on extra hyphens and set truncated hotkey trigger if so.
+   const trigger =
+      ticket.match(/-/g).length > 1
+        ? `${ticket.split("-")[0]}${ticket.split("-")[1]}`
+        : ticket;
+
+    console.log(`${hotstring(trigger, "file")}${file}`);
+    console.log(`${hotstring(trigger, "git")}${string}`);
+    console.log(`${hotstring(trigger)}${ticket}`);
     console.log(`\n`);
   } else {
+    // Entry is a file style string
     entry = entry.split(" - ");
     let [type, ticket, description] = entry;
     const file = `${ticket} - ${description}`;
@@ -46,7 +55,7 @@ async function process(tickets) {
   let folders = [];
 
   printHotstrings(tickets, folders);
-  await makeFolders(folders);
+  // await makeFolders(folders);
 }
 function printHotstrings(tickets, folders) {
   for (const string of tickets) {
