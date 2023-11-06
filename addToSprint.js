1c76e52c1691a0672c2252ea64e156991f1616c3
Kristen San Martin <ksanmartin@filamentgames.com>
2023-11-06 13:00:17 -0800

    11-06 - addToSprint - log folder creation error

--- addToSprint.js
+++ addToSprint.js
@@ -1,25 +1,27 @@
 const util = require("util");
+const { execPromise } = require("./utils");
+const { Messages } = require("./constants");
 const exec = util.promisify(require("child_process").exec);
 const BASE_RESOURCES_FOLDER = `D:\\Users\\Filament_Games\\Documents\\ProjectResources`;
 
 // const PROJECT_FOLDER = `${BASE_RESOURCES_FOLDER}\\CSAR\\v1.40\\Tickets\\sprint-5`;
 // const PROJECT_FOLDER= `${BASE_RESOURCES_FOLDER}\\CASA\\Tickets`
-const PROJECT_FOLDER= `${BASE_RESOURCES_FOLDER}\\FGW\\Tickets`
+const PROJECT_FOLDER = `${BASE_RESOURCES_FOLDER}\\FGW\\Tickets`;
 
-const newTickets = [
-  "Improvement/FGW-322/setup-pipeline-in-gitlab",
-];
+const newTickets = ["Defect/CSAR-952-CSAR-954/WSOD-defensive-approach"];
 
 function categorize(string, folders) {
   let entry = string;
-
-  const hotstring = (ticket, type = "") => `::${ticket.toLowerCase().split("-").join("")}${type}::`;
+  const date = new Date();
+  const hotstring = (ticket, type = "") =>
+    `::${ticket.toLowerCase().split("-").join("")}${type}::`;
 
   if (entry.includes("/")) {
     entry = entry.split("/");
     let [type, ticket, description] = entry;
     const file = `${ticket} - ${description.split("-").join(" ")}`;
     folders.push(`${PROJECT_FOLDER}\\"${file}"`);
+    console.log(`# ${date.toISOString().split("T")[0]} \n`);
     console.log(`${hotstring(ticket, "file")}${file}`);
     console.log(`${hotstring(ticket, "git")}${string}`);
     console.log(`${hotstring(ticket)}${ticket}`);
@@ -40,21 +42,25 @@ function categorize(string, folders) {
   }
 }
 
-function process(tickets) {
+async function process(tickets) {
   let folders = [];
 
   printHotstrings(tickets, folders);
-  makeFolders(folders);
+  await makeFolders(folders);
 }
 function printHotstrings(tickets, folders) {
   for (const string of tickets) {
     categorize(string, folders);
   }
 }
-function makeFolders(folders) {
-  const command = "mkdir " + folders.join(" ");
-  const promise = exec(command);
-  const child = promise.child;
+async function makeFolders(folders) {
+  try {
+    const command = "mkdir " + folders.join(" ");
+    await execPromise(command);
+    console.log(Messages.FOLDERS_CREATED);
+  } catch (err) {
+    console.log(Messages.FOLDER_CREATE_ERROR);
+  }
 }
 
 process(newTickets);
